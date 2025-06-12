514346f7191937358d324c4c407889d6f4cb4fe7
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-06-12 17:03:45 -0500

    chore: initial commit

---
+++ src/main/java/com/acme/agrodigitalbackend/AgroDigitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.acme.agrodigitalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroDigitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroDigitalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/agrodigitalbackend/AgroDigitalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.agrodigitalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroDigitalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
