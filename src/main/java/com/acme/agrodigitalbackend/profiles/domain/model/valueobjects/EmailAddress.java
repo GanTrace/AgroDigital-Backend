b87e348cde72a97e6432109312ff338b872463bb
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-06-13 00:01:53 -0500

    feat(profiles): add value objects for email, name and address

---
+++ src/main/java/com/acme/agrodigitalbackend/profiles/domain/model/valueobjects/EmailAddress.java
@@ -0,0 +1,10 @@
+package com.acme.agrodigitalbackend.profiles.domain.model.valueobjects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.Email;
+
+@Embeddable
+public record EmailAddress(@Email String address) {
+    public EmailAddress() { this(null); }
+}
+
