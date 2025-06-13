5a94acf54a96ae056caf2fa7bfe02d48ae597d57
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-06-13 01:14:57 -0500

    feat(learning): add query records and value object for student data

---
+++ src/main/java/com/acme/agrodigitalbackend/learning/domain/model/queries/GetStudentByProfileIdQuery.java
@@ -0,0 +1,7 @@
+package com.acme.agrodigitalbackend.learning.domain.model.queries;
+
+import com.acme.agrodigitalbackend.learning.domain.model.valueobjects.ProfileId;
+
+public record GetStudentByProfileIdQuery(ProfileId profileId) {
+}
+
