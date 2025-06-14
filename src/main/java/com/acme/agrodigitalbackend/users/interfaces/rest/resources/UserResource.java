f970be4c6c86e59b5341b77483f31ceb6e9d3c3d
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-06-14 15:42:57 -0500

    feat(users): add profileImage field to user resources

--- src/main/java/com/acme/agrodigitalbackend/users/interfaces/rest/resources/UserResource.java
+++ src/main/java/com/acme/agrodigitalbackend/users/interfaces/rest/resources/UserResource.java
@@ -1,4 +1,4 @@
 package com.acme.agrodigitalbackend.users.interfaces.rest.resources;
 
-public record UserResource(Long id, String name, String email, String password, String role) {
+public record UserResource(Long id, String name, String email, String password, String role, String profileImage) {
 }
