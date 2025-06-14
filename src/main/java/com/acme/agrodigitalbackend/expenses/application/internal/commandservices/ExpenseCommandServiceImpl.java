044afdad4f56472a97e7ec47b8a83f07505a7252
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-06-14 01:44:13 -0500

    feat(expenses): implement command and query services for expense management

---
+++ src/main/java/com/acme/agrodigitalbackend/expenses/application/internal/commandservices/ExpenseCommandServiceImpl.java
@@ -0,0 +1,23 @@
+package com.acme.agrodigitalbackend.expenses.application.internal.commandservices;
+
+import com.acme.agrodigitalbackend.expenses.domain.model.aggregates.Expense;
+import com.acme.agrodigitalbackend.expenses.domain.model.commands.CreateExpenseCommand;
+import com.acme.agrodigitalbackend.expenses.domain.services.ExpenseCommandService;
+import com.acme.agrodigitalbackend.expenses.infrastructure.persistence.jpa.repositories.ExpenseRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExpenseCommandServiceImpl implements ExpenseCommandService {
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseCommandServiceImpl(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    @Override
+    public Long handle(CreateExpenseCommand command) {
+        var expense = new Expense(command);
+        expenseRepository.save(expense);
+        return expense.getId();
+    }
+}
