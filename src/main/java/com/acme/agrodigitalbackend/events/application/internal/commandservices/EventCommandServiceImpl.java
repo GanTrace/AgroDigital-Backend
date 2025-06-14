ad46418eb1c142a91f5406688ba91e44f3e8ee7a
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-06-13 19:20:14 -0500

    feat(events): add command and query services for event management

---
+++ src/main/java/com/acme/agrodigitalbackend/events/application/internal/commandservices/EventCommandServiceImpl.java
@@ -0,0 +1,26 @@
+package com.acme.agrodigitalbackend.events.application.internal.commandservices;
+
+import com.acme.agrodigitalbackend.events.domain.model.aggregates.Event;
+import com.acme.agrodigitalbackend.events.domain.model.commands.CreateEventCommand;
+import com.acme.agrodigitalbackend.events.domain.services.EventCommandService;
+import com.acme.agrodigitalbackend.events.infrastructure.persistence.jpa.repositories.EventRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EventCommandServiceImpl implements EventCommandService {
+
+    private final EventRepository eventRepository;
+
+    public EventCommandServiceImpl(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
+
+    @Override
+    public Optional<Event> handle(CreateEventCommand command) {
+        var event = new Event(command);
+        var createdEvent = eventRepository.save(event);
+        return Optional.of(createdEvent);
+    }
+}
