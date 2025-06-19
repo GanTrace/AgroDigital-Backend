e56f552a838fc251babd41907f8e111dfd4d1aaf
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-06-18 19:09:19 -0500

    feat(transactions): add delete transaction endpoint

--- src/main/java/com/acme/agrodigitalbackend/transactions/interfaces/rest/TransactionsController.java
+++ src/main/java/com/acme/agrodigitalbackend/transactions/interfaces/rest/TransactionsController.java
@@ -1,9 +1,13 @@
 package com.acme.agrodigitalbackend.transactions.interfaces.rest;
 
 import com.acme.agrodigitalbackend.expenses.domain.services.ExpenseQueryService;
+import com.acme.agrodigitalbackend.expenses.domain.services.ExpenseCommandService;
 import com.acme.agrodigitalbackend.expenses.domain.model.queries.GetExpensesByUserIdQuery;
+import com.acme.agrodigitalbackend.expenses.domain.model.commands.DeleteExpenseCommand;
 import com.acme.agrodigitalbackend.incomes.domain.services.IncomeQueryService;
+import com.acme.agrodigitalbackend.incomes.domain.services.IncomeCommandService;
 import com.acme.agrodigitalbackend.incomes.domain.model.queries.GetIncomesByUserIdQuery;
+import com.acme.agrodigitalbackend.incomes.domain.model.commands.DeleteIncomeCommand;
 import com.acme.agrodigitalbackend.transactions.interfaces.rest.resources.TransactionResource;
 import com.acme.agrodigitalbackend.transactions.interfaces.rest.transform.TransactionResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -16,17 +20,22 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "*", methods = { RequestMethod.GET })
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.DELETE })
 @RestController
 @RequestMapping(value = "/api/v1/transactions", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Transactions", description = "Transaction Management Endpoints")
 public class TransactionsController {
     private final IncomeQueryService incomeQueryService;
     private final ExpenseQueryService expenseQueryService;
+    private final IncomeCommandService incomeCommandService;
+    private final ExpenseCommandService expenseCommandService;
 
-    public TransactionsController(IncomeQueryService incomeQueryService, ExpenseQueryService expenseQueryService) {
+    public TransactionsController(IncomeQueryService incomeQueryService, ExpenseQueryService expenseQueryService,
+                                IncomeCommandService incomeCommandService, ExpenseCommandService expenseCommandService) {
         this.incomeQueryService = incomeQueryService;
         this.expenseQueryService = expenseQueryService;
+        this.incomeCommandService = incomeCommandService;
+        this.expenseCommandService = expenseCommandService;
     }
 
     @GetMapping("/user/{userId}/recent")
@@ -59,5 +68,23 @@ public class TransactionsController {
             .collect(Collectors.toList());
         
         return ResponseEntity.ok(recentTransactions);
+    }
+
+    @DeleteMapping("/{transactionId}")
+    public ResponseEntity<Void> deleteTransaction(@PathVariable Long transactionId, @RequestParam String type) {
+        try {
+            if ("income".equalsIgnoreCase(type)) {
+                var deleteIncomeCommand = new DeleteIncomeCommand(transactionId);
+                incomeCommandService.handle(deleteIncomeCommand);
+            } else if ("expense".equalsIgnoreCase(type)) {
+                var deleteExpenseCommand = new DeleteExpenseCommand(transactionId);
+                expenseCommandService.handle(deleteExpenseCommand);
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
